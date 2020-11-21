b9cdaf0d1dfcffb10d355b78c3d0c1ce0d3095a3
Tim <16307130090@fudan.edu.cn>
2020-11-21 19:51:37 +0800

    wrap with a function

--- strat.py
+++ strat.py
@@ -12,108 +12,112 @@ from statsmodels.tsa.stattools import adfuller
 from sklearn.linear_model import LinearRegression
 from scipy.stats import shapiro
 
-first_data = pd.read_csv("../future_by_date/20190612.csv")
-instruments = list(set(first_data['windcode']))
-n_instr = len(instruments)
+def regress_by_date(last_date):
+    first_data = pd.read_csv("../future_by_date/{}.csv".format(last_date))
+    instruments = list(set(first_data['windcode']))
+    n_instr = len(instruments)
 
-#  def extract_date(s):
-    #  t = s['Unnamed: 0']
-    #  return t.year*10000 + t.month*100 + t.day
+    #  def extract_date(s):
+            #  t = s['Unnamed: 0']
+            #  return t.year*10000 + t.month*100 + t.day
 
-#  def extract_time(s):
-    #  t = s['Unnamed: 0']
-    #  return t.hour*100 + t.minute
+    #  def extract_time(s):
+            #  t = s['Unnamed: 0']
+            #  return t.hour*100 + t.minute
 
-# check for conintegration pairs
-clist = []
-for x in range(n_instr):
-    for y in range(x+1,n_instr):
-        i = instruments[x]
-        j = instruments[y]
-        #  future = pd.DataFrame()
-        #  print("Doing:",i,j)
-        future0 = first_data[first_data['windcode']==i][['time','close']]
-        #  future0.columns=['time0','close0']
+    #  check for conintegration pairs
+    clist = []
+    for x in range(n_instr):
+        for y in range(x+1,n_instr):
+            i = instruments[x]
+            j = instruments[y]
+            #  future = pd.DataFrame()
+            #  print("Doing:",i,j)
+            future0 = first_data[first_data['windcode']==i][['time','close']]
+            #  future0.columns=['time0','close0']
+            future0.fillna(method='ffill',inplace=True)
+            future1 = first_data[first_data['windcode']==j][['time','close']]
+            future1.fillna(method='ffill',inplace=True)
+            #  future1.columns=['time1','close1']
+            # max_lag: 0
+            # if set to 1 or larger, more of them can pass the test
+            result = coint(future0['close'],future1['close'])
+            clist.append((i,j,result[1]))
+
+    #  clist = sorted(clist,key=lambda x:x[2])
+
+    #  for i in clist:
+            #  if i[2] < 0.05:
+                #  print(i)
+
+    #  test and plot area
+    #  i,j = 'SR909.CZC', 'AL1905.SHF'
+    #  i,j = 'PP1905.DCE', 'A1905.DCE'
+    #  future0 = first_data[first_data['windcode']==i][['time','close']]
+    #  future1 = first_data[first_data['windcode']==j][['time','close']]
+    #  future0.fillna(method='ffill',inplace=True)
+    #  future1.fillna(method='ffill',inplace=True)
+
+    #  d0 = future0['close'].to_numpy()
+    #  d1 = future1['close'].to_numpy()
+
+    #  d0 -= np.average(d0)
+    #  d1 -= np.average(d1)
+
+    #  d0 /= np.std(d0)
+    #  d1 /= np.std(d1)
+
+    #  plt.plot(d0)
+    #  plt.plot(d1)
+
+    #  add one section selecting pairs
+
+    #  i, j = 'AL1908.SHF', 'P1909.DCE'
+
+    for i,j,coint_p in clist:
+        if coint_p >= 0.05:
+            continue
+        print("Pair Anlysis:",i,j)
+        future0 = first_data[first_data['windcode']==i]['close'].copy()
+        future1 = first_data[first_data['windcode']==j]['close'].copy()
         future0.fillna(method='ffill',inplace=True)
-        future1 = first_data[first_data['windcode']==j][['time','close']]
         future1.fillna(method='ffill',inplace=True)
-        #  future1.columns=['time1','close1']
-        # max_lag: 0
-        # if set to 1 or larger, more of them can pass the test
-        result = coint(future0['close'],future1['close'])
-        clist.append((i,j,result[1]))
 
-#  clist = sorted(clist,key=lambda x:x[2])
+        future0 = future0.to_numpy().reshape(-1,1)
+        future1 = future1.to_numpy()
 
-#  for i in clist:
-    #  if i[2] < 0.05:
-        #  print(i)
+        reg = LinearRegression().fit(future0,future1)
+        print("R^2:", reg.score(future0,future1))
+        print("coef:", reg.coef_, "intercept:", reg.intercept_)
 
-#  test and plot area
-#  i,j = 'SR909.CZC', 'AL1905.SHF'
-#  i,j = 'PP1905.DCE', 'A1905.DCE'
-#  future0 = first_data[first_data['windcode']==i][['time','close']]
-#  future1 = first_data[first_data['windcode']==j][['time','close']]
-#  future0.fillna(method='ffill',inplace=True)
-#  future1.fillna(method='ffill',inplace=True)
+        residue = future1 - reg.predict(future0)
 
-#  d0 = future0['close'].to_numpy()
-#  d1 = future1['close'].to_numpy()
+        #  print("------------------------ Residue ------------------------")
+        #  print(residue)
+        #  print("------------------------ Residue ------------------------")
 
-#  d0 -= np.average(d0)
-#  d1 -= np.average(d1)
+        _, adftest_result, *_ = adfuller(residue)
 
-#  d0 /= np.std(d0)
-#  d1 /= np.std(d1)
-
-#  plt.plot(d0)
-#  plt.plot(d1)
-
-# add one section selecting pairs
-
-#  i, j = 'AL1908.SHF', 'P1909.DCE'
-
-for i,j,coint_p in clist:
-    if coint_p >= 0.05:
-        continue
-    print("Pair Anlysis:",i,j)
-    future0 = first_data[first_data['windcode']==i]['close'].copy()
-    future1 = first_data[first_data['windcode']==j]['close'].copy()
-    future0.fillna(method='ffill',inplace=True)
-    future1.fillna(method='ffill',inplace=True)
-
-    future0 = future0.to_numpy().reshape(-1,1)
-    future1 = future1.to_numpy()
-
-    reg = LinearRegression().fit(future0,future1)
-    print("R^2:", reg.score(future0,future1))
-    print("coef:", reg.coef_, "intercept:", reg.intercept_)
-
-    residue = future1 - reg.predict(future0)
-
-    #  print("------------------------ Residue ------------------------")
-    #  print(residue)
-    #  print("------------------------ Residue ------------------------")
-
-    _, adftest_result, *_ = adfuller(residue)
-
-    if adftest_result < 0.05:
-        _, shapiro_result, *_ = shapiro(residue)
-        print("p-value for normality test:", shapiro_result)
-        if shapiro_result >= 0.05:
-            print("Non normal residue found")
+        if adftest_result < 0.05:
+            _, shapiro_result, *_ = shapiro(residue)
+            print("p-value for normality test:", shapiro_result)
+            if shapiro_result >= 0.05:
+                print("Non normal residue found")
+            else:
+                # generate signal
+                # TODO Add moving average?
+                z_residue = (residue - np.mean(residue)) / np.std(residue)
+                print(z_residue)
         else:
-            # generate signal
-            # TODO Add moving average?
-            z_residue = (residue - np.mean(residue)) / np.std(residue)
-            print(z_residue)
-    else:
-        print("Non-stantionary cointegration detected!")
-        print("p-value for ADF test:", adftest_result)
-    break
+            print("Non-stantionary cointegration detected!")
+            print("p-value for ADF test:", adftest_result)
+        break
 
 #  test plot of residue
 #  plt.plot(residue)
 #  plt.show()
 
 # 收益补充
+if __name__ == "__main__":
+    f = 20190612
+    regress_by_date(f)
